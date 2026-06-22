1f705da609fcf3f42d8ccd239a78851d6ae155da
Sanket Kumar <ssbkumar5513@gmail.com>
2026-06-22 16:15:20 +0530

    sanket

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,11 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', '
     Route::post('epapers/media', 'EpaperApiController@storeMedia')->name('epapers.storeMedia');
     Route::apiResource('epapers', 'EpaperApiController');
 });
+
+Route::group([
+    'namespace' => 'Api\V1\Admin'
+], function () {
+
+    Route::post('heartbeat', 'DeviceApiController@heartbeat');
+    Route::post('attendance', 'DeviceApiController@attendance');
+});
